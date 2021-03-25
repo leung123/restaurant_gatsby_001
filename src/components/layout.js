a5a1d36887746fc32cf46438f15648cc984151dd
leung123 <leung123msn@hotmail.com>
2021-03-26 02:26:53 +0800

    saved

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,35 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
+import Navbar from './globals/navbar/Navbar';
+import Footer from '../components/globals/Footer';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </React.Fragment>
+    );
+};
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
+body {
+    font-family: 'Open Sans', sans-serif;
+    color: #262626;
+    background: #fff;
+}
+`;
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,22 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../components/layout';
+import { HomeHeader, Banner, BannerButton } from '../utils';
+import img from '../images/bcg/homeBcg.jpg';
+import SEO from '../components/seo';
+import QuickInfo from '../components/HomeComponents/QuickInfo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+    <Layout>
+        <SEO title="Home" />
+        <HomeHeader img={img}>
+            <Banner title="Fine Dining" subtitle="65, MG Road- Bangalore, KA">
+                <BannerButton style={{ margin: '2rem auto' }}>
+                    menu
+                </BannerButton>
+            </Banner>
+        </HomeHeader>
+        <QuickInfo />
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
